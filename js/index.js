a16aff4d493ec192f278c10f3330f4ac9e15c22d
Giacomo Benati <giacomobenati@mailbox.org>
2019-01-28 15:42:12 +0100

    reformatted the page to be more consistent

--- js/index.js
+++ js/index.js
@@ -41,8 +41,8 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-// not the cleanest solution, but it let me loop inside of "navAnchors" with the new appended items.
-//I should probably reformat all the page to let it be consistent with itself, but I think it's good for now.
+//I still prefer to declare all vars at begenning, but sometimes it's necessary to declar them as you go, specially if you add things to the html and need to select the new elemnets inside your nodes.
+//nav
 const nav = document.querySelector('nav');
 const newNavAnchor = document.createElement('a');
 newNavAnchor.href = "#";
@@ -51,18 +51,6 @@ const copyOfNNA = newNavAnchor.cloneNode(true);
 nav.prepend(newNavAnchor);
 nav.appendChild(copyOfNNA);
 const navAnchors = nav.querySelectorAll('a');
-
-const ctaImg = document.querySelector('#cta-img');
-const cta = document.querySelector('.cta');
-const h1 = document.querySelector('h1');
-const ctaButton = cta.querySelector('button');
-const h4s = document.querySelectorAll('h4');
-const paragraphs = document.querySelectorAll('p');
-const middleImg = document.querySelector('.middle-img');
-
-
-
-//nav
 navAnchors[1].textContent = "Services";
 navAnchors[2].textContent = "Product";
 navAnchors[3].textContent = "Vision";
@@ -71,20 +59,29 @@ navAnchors[5].textContent = "About";
 navAnchors[6].textContent = "Contact";
 navAnchors.forEach(element => element.style = "color: green");
 
-
 //cta
+const ctaImg = document.querySelector('#cta-img');
+const cta = document.querySelector('.cta');
+const h1 = document.querySelector('h1');
+const ctaButton = cta.querySelector('button');
 ctaImg.src = "img/header-img.png";
 h1.style = 'white-space: pre';
 h1.textContent = "DOM\r\n Is\r\n Awesome";
 //h1.innerHTML = "DOM<br> Is<br> Awesome"; //With this we use innerHTML, bad!
 ctaButton.textContent = "Get Started";
+
+const h4s = document.querySelectorAll('h4');
+const paragraphs = document.querySelectorAll('p');
 //top section
 h4s[0].textContent = "Features";
 paragraphs[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
 h4s[1].textContent = "About";
 paragraphs[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
 //middle img
 middleImg.src = "img/mid-page-accent.jpg";
+const middleImg = document.querySelector('.middle-img');
+
 //bottom section
 h4s[2].textContent = "Services";
 paragraphs[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
@@ -92,6 +89,7 @@ h4s[3].textContent = "Product";
 paragraphs[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
 h4s[4].textContent = "Vision";
 paragraphs[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
 //contact
 h4s[5].textContent = "Contact";
 paragraphs[5].style = 'white-space: pre';
@@ -99,5 +97,6 @@ paragraphs[5].textContent = "123 Way 456 Street\r\nSomewhere, USA";
 //paragraphs[5].innerHTML = "123 Way 456 Street<br>Somewhere, USA"; //With this we use innerHTML, bad!
 paragraphs[6].textContent = "1 (888) 888-8888";
 paragraphs[7].textContent = "sales@greatidea.io";
+
 //footer
 paragraphs[8].textContent = "Copyright Great Idea! 2018";
