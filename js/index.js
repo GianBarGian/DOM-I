66158cf623e5cac8f2001f4fe7d2265baf4e633c
Giacomo Benati <giacomobenati@mailbox.org>
2019-01-28 14:26:20 +0100

    (MVP) Updated index.html = original.html

--- js/index.js
+++ js/index.js
@@ -41,3 +41,46 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+const nav = document.querySelector('nav');
+const navAnchors = nav.querySelectorAll('a');
+const ctaImg = document.querySelector('#cta-img');
+const cta = document.querySelector('.cta');
+const h1 = document.querySelector('h1');
+const ctaButton = cta.querySelector('button');
+const h4s = document.querySelectorAll('h4');
+const paragraphs = document.querySelectorAll('p');
+const middleImg = document.querySelector('.middle-img');
+
+//nav
+navAnchors[0].textContent = "Services";
+navAnchors[1].textContent = "Product";
+navAnchors[2].textContent = "Vision";
+navAnchors[3].textContent = "Features";
+navAnchors[4].textContent = "About";
+navAnchors[5].textContent = "Contact";
+//cta
+ctaImg.src = "img/header-img.png";
+//h1.textContent = "DOM\r\n Is\r\n Awesome";
+h1.innerHTML = "DOM<br> Is<br> Awesome";
+ctaButton.textContent = "Get Started";
+//top section
+h4s[0].textContent = "Features";
+paragraphs[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+h4s[1].textContent = "About";
+paragraphs[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+//middle img
+middleImg.src = "img/mid-page-accent.jpg";
+//bottom section
+h4s[2].textContent = "Services";
+paragraphs[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+h4s[3].textContent = "Product";
+paragraphs[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+h4s[4].textContent = "Vision";
+paragraphs[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+//contact
+h4s[5].textContent = "Contact";
+paragraphs[5].innerHTML = "123 Way 456 Street<br>Somewhere, USA";
+paragraphs[6].textContent = "1 (888) 888-8888";
+paragraphs[7].textContent = "sales@greatidea.io";
+//footer
+paragraphs[8].textContent = "Copyright Great Idea! 2018";
