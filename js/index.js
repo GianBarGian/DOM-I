cdf69410da2709f55d3aebd08e0efba83411b2b7
Giacomo Benati <giacomobenati@mailbox.org>
2019-01-28 16:38:29 +0100

    small format renovation

--- js/index.js
+++ js/index.js
@@ -67,15 +67,17 @@ const ctaButton = cta.querySelector('button');
 ctaImg.src = "img/header-img.png";
 h1.style = 'white-space: pre';
 h1.textContent = "DOM\r\n Is\r\n Awesome";
-//h1.innerHTML = "DOM<br> Is<br> Awesome"; //With this we use innerHTML, bad!
+  //h1.innerHTML = "DOM<br> Is<br> Awesome"; //With this we use innerHTML, bad!
 ctaButton.textContent = "Get Started";
 let ctaButtonEvent = event => {
   return event.target.style ="background: blue";
 }
 ctaButton.addEventListener('click', ctaButtonEvent);
 
+
 const h4s = document.querySelectorAll('h4');
 const paragraphs = document.querySelectorAll('p');
+
 //top section
 h4s[0].textContent = "Features";
 paragraphs[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
@@ -99,7 +101,7 @@ paragraphs[4].textContent = "Vision content elementum magna eros, ac posuere elv
 h4s[5].textContent = "Contact";
 paragraphs[5].style = 'white-space: pre';
 paragraphs[5].textContent = "123 Way 456 Street\r\nSomewhere, USA";
-//paragraphs[5].innerHTML = "123 Way 456 Street<br>Somewhere, USA"; //With this we use innerHTML, bad!
+  //paragraphs[5].innerHTML = "123 Way 456 Street<br>Somewhere, USA"; //With this we use innerHTML, bad!
 paragraphs[6].textContent = "1 (888) 888-8888";
 paragraphs[7].textContent = "sales@greatidea.io";
 
